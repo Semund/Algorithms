4f79dc17afadd609af81703dbe8521d5a019b4f8
Semund <semund.sigurdsson@gmail.com>
2021-10-29 18:12:09 +0300

    Create binary search for digits or letters.

---
+++ binarysearch.py
@@ -0,0 +1,23 @@
+import random
+
+
+def binary_search(lst: iter, obj: int or str) -> int or None:
+    lst_sorted = sorted(list(lst))
+    low = 0
+    high = len(lst_sorted)
+    while low <= high:
+        med = (low + high) // 2
+        if lst_sorted[med] == obj:
+            return med
+        elif lst_sorted[med] > obj:
+            high = med - 1
+        else:
+            low = med + 1
+    return None
+
+
+lst = (random.randint(1, 50) for _ in range(50))
+# lst = sorted(list('fwelkjfqwoeivnawoqnqoiwerjvqweironvoqweirhvnqweivnedovniaavndvaddsvweiuvnwdsv'))
+obj = random.randint(1, 50)
+# obj = random.choice(lst)
+print(binary_search(lst, obj))
