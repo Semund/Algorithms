53a08b9088d8b89ff114ae18fe9ac8ad9ffd374b
Semund <semund.sigurdsson@gmail.com>
2021-11-13 17:45:00 +0300

    Added element insertion to the concrete position of the linked list

--- OTUS_Algorithms/datastructure/slinkedlist.py
+++ OTUS_Algorithms/datastructure/slinkedlist.py
@@ -44,6 +44,25 @@ class SLinkedList:
             self._head = new_item
         self._lenght += 1
 
+    def append_position(self, item, index):
+        if index > self._lenght:
+            raise IndexError
+        new_item = NodeList(item)
+        i = 0
+        current_item = self._head
+        prev_item = None
+        while i < index:
+            prev_item = current_item
+            current_item = current_item.next
+            i += 1
+        if prev_item is None:
+            new_item.next = self._head
+            self._head = new_item
+        else:
+            prev_item.next = new_item
+            new_item.next = current_item
+        self._lenght += 1
+
     def __len__(self):
         return self._lenght
 
@@ -73,3 +92,11 @@ if __name__ == '__main__':
     for j in range(11, 20):
         my_llist.append_head(j)
     print(my_llist)
+    my_llist.append_position(100, 10)
+    print(my_llist)
+    my_llist.append_position(200, 0)
+    print(my_llist)
+    llist = SLinkedList()
+    llist.append_position(1, 0)
+    llist.append_position(2, 1)
+    print(llist)
