cd9e717eae6808b432c4b9d529e6b6a7d7dbbfc5
Semyon Fedorov <semund.sigurdsson@gmail.com>
2021-11-22 17:21:48 +0300

    Added a generator of permutations of N-numbers in M-positions

---
+++ OTUS_Algorithms/permutations.py
@@ -0,0 +1,17 @@
+def generate_permutations(n: int, m: int, prefix=None):
+    """Generating all possible permutations of n-numbers in m-position with leading zeros"""
+    if m == 0:
+        print(*prefix, end=', ', sep='')
+        return
+    prefix = prefix or []
+    for digit in range(n):
+        if digit in prefix:
+            continue
+        prefix.append(digit)
+        generate_permutations(n, m - 1, prefix)
+        prefix.pop()
+
+
+generate_permutations(10, 6)
+
+
