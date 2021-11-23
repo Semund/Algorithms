2aa93a30926b59c766583d8b912ee8b1eaf93f1f
Semund <semund.sigurdsson@gmail.com>
2021-11-23 19:32:00 +0300

    Added a merge sort

---
+++ Algorithms_build_analyse/sorting/merge.py
@@ -0,0 +1,34 @@
+from random import randint
+
+
+def merge(left: list, right: list):
+    n = len(left)
+    m = len(right)
+    i, j = 0, 0
+    result = []
+    while i < n and j < m:
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    if i == n:
+        result.extend(right[j:])
+    elif j == m:
+        result.extend(left[i:])
+    return result
+
+
+def merge_sort(nums):
+    if len(nums) < 2:
+        return nums
+    else:
+        pivot = len(nums) // 2
+        left = merge_sort(nums[:pivot])
+        right = merge_sort(nums[pivot:])
+        return merge(left, right)
+
+
+nums = [randint(1, 10000) for _ in range(10000)]
+print(merge_sort(nums) == sorted(nums))
